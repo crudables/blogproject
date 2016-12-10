5a58cdf1e466e8cc283a5a9a3649a91c09cd083f
ables <crudables@yahoo.com>
2016-12-10 17:05:51 +0100

    a new commit

--- blog/models.py
+++ blog/models.py
@@ -29,5 +29,4 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post_detail',args=[self.publish.year,self.publish.strftime('%m'),
-                        self.publish.strftime('%d'),
-                        self.slug])
+                        self.publish.strftime('%d'),self.slug])
