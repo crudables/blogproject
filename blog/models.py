2a7ca31d56053d4176482070f93caedf60d4cb39
ables <crudables@yahoo.com>
2016-12-12 17:27:04 +0100

    added comment

--- blog/models.py
+++ blog/models.py
@@ -2,12 +2,16 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from taggit.managers import TaggableManager
 
 # Create your models here.
+
+#model manager for model Post
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super(PublishedManager,self).get_queryset().filter(status='published')
 
+#model for Post
 class Post(models.Model):
     STATUS_CHOICES =(('draft','Draft'),('published','Published'),)
     title = models.CharField(max_length=250)
@@ -18,6 +22,7 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add = True)
     updated = models.DateTimeField(auto_now_add = True)
     status = models.CharField(max_length=10, choices = STATUS_CHOICES, default = 'draft')
+    tagsx = TaggableManager()
 
     class Meta:
         ordering = ('-publish',)
@@ -30,3 +35,19 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('blog:post_detail',args=[self.publish.year,self.publish.strftime('%m'),
                         self.publish.strftime('%d'),self.slug])
+
+
+#model for comments
+class Comment(models.Model):
+    post = models.ForeignKey(Post,related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add = True)
+    updated = models.DateTimeField(auto_now_add = True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.name,self.post)
--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,12 @@ from .models import Post
 from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
 from .forms import EmailPostForm
 from django.core.mail import send_mail
+from .models import Comment
+from .forms import CommentForm
 
 # Create your views here.
 def post_list(request):
-    posts_list = Post.objects.all()
+    posts_list = Post.published.all()
     paginator = Paginator(posts_list, 3) #3 post in each Page
     page = request.GET.get('page')
     try:
@@ -20,9 +22,25 @@ def post_list(request):
 
     return render(request,'blog/post/list.html',{'page':page, 'posts':posts})
 def post_detail(request,year,month,day,post):
+    #list of post with given criteria
     post = get_object_or_404(Post, slug=post,status='published',publish__year=year,
     publish__month=month,publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post':post})
+    #list of active post for above comment
+    comments = post.comments.filter(active=True)
+    if request.method == 'POST':
+        #a comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            #create comment object but dont dave to database yet
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+
+            #save comment to DB
+            new_comment.save()
+            comment_form = CommentForm()
+    else:
+        comment_form = CommentForm()
+    return render(request, 'blog/post/detail.html', {'post':post, 'comment_form':comment_form,'comments':comments})
 
 
 def post_share(request,post_id):
