185366bc46889a651a62a7e4cd3bf58b711ae270
crudables <crudables@yahoo.com>
2016-12-13 16:29:00 +0100

    sss

--- blog/views.py
+++ blog/views.py
@@ -25,7 +25,7 @@ def post_list(request,tag_slug=None):
         # if page is out of range, deliver last page of result
         posts = paginator.page(paginator.num_pages)
 
-    return render(request,'blog/post/list.html',{'page':page, 'posts':posts})
+    return render(request,'blog/post/list.html',{'page':page, 'posts':posts,'tag':tag})
 def post_detail(request,year,month,day,post):
     #list of post with given criteria
     post = get_object_or_404(Post, slug=post,status='published',publish__year=year,
